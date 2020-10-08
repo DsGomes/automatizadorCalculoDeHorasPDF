c88e1f859fd8c212ed99cb4548b9461c49e9f184
Alvaro Rezende <alvarorezende@momentum.com.br>
2020-10-08 09:45:05 -0300

    Change palavraChave array to another file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
+const { palavraChave } = require('./palavrasChave');
 
-function render_page(pageData){
+function render_page(pageData) {
     let render_options = {
         normalizeWhitespace: false,
         disableCombineTextItems: false
@@ -21,42 +22,6 @@ let options = {
     pagerender: render_page
 }
 
-let palavraChave = [
-            {atividade: "simples de API", value: 1},
-            {atividade: "média de API", value: 3},
-            {atividade: "complexa de API", value: 6},
-            {atividade: "simples de console", value: 3},
-            {atividade: "média de console", value: 8},
-            {atividade: "complexa de console", value: 12},
-            {atividade: "simples de procedure", value: 1},
-            {atividade: "média de procedure", value: 4},
-            {atividade: "complexa de procedure", value: 8},
-            {atividade: "simples de tabela", value: 1},
-            {atividade: "média de tabela", value: 3},
-            {atividade: "complexa de tabela", value: 6},
-            {atividade: "simples de formulário", value: 1},
-            {atividade: "média de formulário", value: 3},
-            {atividade: "complexa de formulário", value: 6},
-            {atividade: "simples de job", value: 2},
-            {atividade: "média de job", value: 5},
-            {atividade: "complexa de job", value: 10},
-            {atividade: "simples de trigger", value: 1},
-            {atividade: "média de trigger", value: 3},
-            {atividade: "complexa de trigger", value: 6},
-            {atividade: "simples de função", value: 1},
-            {atividade: "média de função", value: 3},
-            {atividade: "complexa de função", value: 6},
-            {atividade: "simples de script", value: 1},
-            {atividade: "média de script", value: 3},
-            {atividade: "complexa de script", value: 6},
-            {atividade: "simples de modelagem", value: 1},
-            {atividade: "média de modelagem", value: 3},
-            {atividade: "complexa de modelagem", value: 6},
-            {atividade: "simples do relatório", value: 3},
-            {atividade: "média do relatório", value: 8},
-            {atividade: "complexa do relatório", value: 12}            
-    ];
-
 let totalDeHoras = 0;
 
 let dataBuffer = fs.readFileSync('./levantamento.pdf');
@@ -64,11 +29,11 @@ let dataBuffer = fs.readFileSync('./levantamento.pdf');
 pdf(dataBuffer, options)
     .then(data => {
         let text = data.text.split(',');
-        text.forEach(item =>{
-            palavraChave.forEach(palavra =>{
+        text.forEach(item => {
+            palavraChave.forEach(palavra => {
                 let itemReplaced = item.replace(/  /g, ' ');
-                if(itemReplaced.indexOf(palavra.atividade) != -1){
-                        totalDeHoras += palavra.value;
+                if (itemReplaced.indexOf(palavra.atividade) != -1) {
+                    totalDeHoras += palavra.value;
                 }
             });
         })
@@ -79,7 +44,7 @@ pdf(dataBuffer, options)
             ['Total de horas desenvolvimento: ', text[text.length - 5]],
             ['Total de horas homologação: ', text[text.length - 3]],
             ['Total geral levantamento: ', text[text.length - 1]],
-        
+
         ])
     })
     .catch(error => {
